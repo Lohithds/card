0067596a5f382dde0a51842086c61d42cd709a62
Lohithds <83329136+Lohithds@users.noreply.github.com>
2021-05-03 13:29:13 +0530

    Add files via upload

---
+++ Card.vue
@@ -0,0 +1,314 @@
+<template>
+    <div >
+  <b-navbar class="nav" toggleable="lg" type="dark" variant="info">
+    <b-navbar-brand >Fans Card</b-navbar-brand>
+  </b-navbar>
+
+  <div>
+    <div class="outer">
+  <div  class="card1">
+  <b-card
+    title="KING KOHLI"
+    img-src="vk.jpg"
+    img-alt="Image"
+    img-top
+    tag="article"
+    style="max-width: 15rem;"
+    class="mb-2"
+  >
+    <b-card-text>
+      <span>
+          Name: Virat Kohli
+      </span><br/>
+       <span>
+          DOB: 5/Nov/1988
+      </span><br/>
+       <span>
+          Age:32
+      </span><br/>
+      
+
+    </b-card-text>
+  </b-card>
+</div>
+<div class="inner">
+ <div  class="card2">
+  <b-card
+    title="Mr COOL"
+    img-src="msdr.jpg"
+    img-alt="Image"
+    img-top
+    tag="article"
+    style="max-width: 15rem;"
+    class="mb-2"
+  >
+    <b-card-text>
+      <span>
+          Name:M S dhoni
+      </span><br/>
+       <span>
+          DOB: 7/july/1981
+      </span><br/>
+       <span>
+          Age:39
+      </span><br/>
+      
+
+    </b-card-text>
+  </b-card>
+
+</div>
+  <div class="inner2">
+ <div  class="card3">
+  <b-card
+    title="HIT MAN"
+    img-src="rohith.jpg"
+    img-alt="Image"
+    img-top
+    tag="article"
+    style="max-width: 15rem;"
+    class="mb-2"
+  >
+    <b-card-text>
+      <span>
+          Name: Rohith Sharma
+      </span><br/>
+       <span>
+          DOB: 30/April/1987
+      </span><br/>
+       <span>
+          Age:34
+      </span><br/>
+      
+
+    </b-card-text>
+  </b-card>
+</div>
+  </div>
+  <div class="=inner3">
+ <div class="card4">
+  <b-card
+    title="Master Blaster"
+    img-src="sachin.jpg"
+    img-alt="Image"
+    img-top
+    tag="article"
+    style="max-width: 15rem;"
+    class="mb-2"
+  >
+    <b-card-text>
+      <span>
+          Name: Sachin Tendulkar 
+      </span><br/>
+       <span>
+          DOB: 24/April/1973
+      </span><br/>
+       <span>
+          Age:48
+      </span><br/>
+      
+
+    </b-card-text>
+  </b-card>
+</div>
+  </div>
+  <div class="inner4">
+ <div class="card5">
+  <b-card
+    title="K L"
+    img-src="rahul.jpg"
+    img-alt="Image"
+    img-top
+    tag="article"
+    style="max-width: 15rem;"
+    class="mb-2"
+  >
+    <b-card-text>
+      <span>
+          Name: K L Rahul
+      </span><br/>
+       <span>
+          DOB: 18/April/1992
+      </span><br/>
+       <span>
+          Age:29
+      </span><br/>
+      
+
+    </b-card-text>
+  
+  </b-card>
+ </div>
+</div>
+</div>
+</div>
+  </div>
+  
+  <div>
+    <div class="outer">
+  <div  class="card1">
+  <b-card
+    title="Mr 360"
+    img-src="ab1.jpg"
+    img-alt="Image"
+    img-top
+    tag="article"
+    style="max-width: 15rem;"
+    class="mb-2"
+  >
+    <b-card-text>
+      <span>
+          Name: AB de Villiers
+      </span><br/>
+       <span>
+          DOB: 17/Feb/1984
+      </span><br/>
+       <span>
+          Age:37
+      </span><br/>
+      
+
+    </b-card-text>
+  </b-card>
+</div>
+<div class="inner">
+ <div  class="card2">
+  <b-card
+    title="WALL"
+    img-src="rahuld.jpg"
+    img-alt="Image"
+    img-top
+    tag="article"
+    style="max-width: 15rem;"
+    class="mb-2"
+  >
+    <b-card-text>
+      <span>
+          Name:Rahul Dravid
+      </span><br/>
+       <span>
+          DOB: 24/jan/1973
+      </span><br/>
+       <span>
+          Age:48
+      </span><br/>
+      
+
+    </b-card-text>
+  </b-card>
+
+</div>
+  <div class="inner2">
+ <div  class="card3">
+  <b-card
+    title="DADA"
+    img-src="ganguly.jpg"
+    img-alt="Image"
+    img-top
+    tag="article"
+    style="max-width: 15rem;"
+    class="mb-2"
+  >
+    <b-card-text>
+      <span>
+          Name: Sourav Ganguly
+      </span><br/>
+       <span>
+          DOB: 8/July/1972
+      </span><br/>
+       <span>
+          Age:48
+      </span><br/>
+      
+
+    </b-card-text>
+  </b-card>
+</div>
+  </div>
+  <div class="=inner3">
+ <div class="card4">
+  <b-card
+    title="NAWAB"
+    img-src="sehwag.jpg"
+    img-alt="Image"
+    img-top
+    tag="article"
+    style="max-width: 15rem;"
+    class="mb-2"
+  >
+    <b-card-text>
+      <span>
+          Name: Virender Sehwag
+      </span><br/>
+       <span>
+          DOB: 20/Oct/1978
+      </span><br/>
+       <span>
+          Age:42
+      </span><br/>
+      
+
+    </b-card-text>
+  </b-card>
+</div>
+  </div>
+  <div class="inner4">
+ <div class="card5">
+  <b-card
+    title="  PAJI"
+    img-src="yuvraj.jpg"
+    img-alt="Image"
+    img-top
+    tag="article"
+    style="max-width: 15rem;"
+    class="mb-2"
+  >
+    <b-card-text>
+      <span>
+          Name: Yuvraj Singh
+      </span><br/>
+       <span>
+          DOB: 12/Dec/1981
+      </span><br/>
+       <span>
+          Age:39
+      </span><br/>
+      
+
+    </b-card-text>
+  
+  </b-card>
+ </div>
+</div>
+</div>
+</div>
+  </div>
+    </div>
+
+</template>
+<script>
+export default {
+    name:"Card"
+}
+</script>
+<style>
+.nav{
+  margin-top:- 100px;
+
+
+}.card1{
+float: left;
+margin-left: 80px;
+
+}.card2{
+  float: left;
+}.card3{
+  float: left;
+}.card4{
+  float: left;
+}.outer{
+  margin-top: 20px;
+}
+
+
+</style>
